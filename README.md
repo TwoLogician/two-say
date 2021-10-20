71b00e0202242e225d24a885f1fe03322cbdbd7f
Two <Two.Logician@icloud.com>
2021-10-21 03:46:00 +0700

    :tada:

---
+++ README.md
@@ -0,0 +1,8 @@
+## TwoSay
+
+Usage: `two [options]`
+
+option | description
+--- | ---
+`date` | Gets a DateTime object that is set to the current date and time on this computer, expressed as the local time.
+`guid` | Initializes a new instance of the Guid structure.
---
+++ src/twodo.say/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace twodo.say
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = new List<string> { "DATE", "GUID" };
+
+            if (!args.Any())
+            {
+                Console.WriteLine("Usage: two [options]");
+                return;
+            }
+            if (!args.Any(x => commands.Contains(x.ToUpper())))
+            {
+                Console.WriteLine("Unknow Option");
+                return;
+            }
+            if (args.Any(x => x.ToUpper() == "DATE"))
+            {
+                Console.WriteLine(DateTime.Now);
+            }
+            if (args.Any(x => x.ToUpper() == "GUID"))
+            {
+                Console.WriteLine(Guid.NewGuid());
+            }
+        }
+    }
+}
