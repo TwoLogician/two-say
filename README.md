64903925dc1847276ce940b0ca2cb0db996e6868
Two <Two.Logician@icloud.com>
2021-10-21 04:23:33 +0700

    :lipstick:

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Usage: `two [options]`
 
 option | description
 --- | ---
+`a-z` | Returns a random character that is within A to Z.
 `date` | Gets a DateTime object that is set to the current date and time on this computer, expressed as the local time.
 `guid` | Initializes a new instance of the Guid structure.
--- src/twodo.say/Program.cs
+++ src/twodo.say/Program.cs
@@ -8,17 +8,28 @@ namespace twodo.say
     {
         static void Main(string[] args)
         {
-            var commands = new List<string> { "DATE", "GUID" };
+            var commands = new List<string> { "A-Z", "DATE", "GUID" };
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
             if (!args.Any())
             {
                 Console.WriteLine("Usage: two [options]");
+                Console.ForegroundColor = ConsoleColor.White;
                 return;
             }
+            Console.ForegroundColor = ConsoleColor.Red;
             if (!args.Any(x => commands.Contains(x.ToUpper())))
             {
                 Console.WriteLine("Unknow Option");
+                Console.ForegroundColor = ConsoleColor.White;
                 return;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (args.Any(x => x.ToUpper() == "A-Z"))
+            {
+                var random = new Random();
+                var cha = (char)random.Next('A', 'Z');
+                Console.WriteLine(cha);
             }
             if (args.Any(x => x.ToUpper() == "DATE"))
             {
@@ -28,6 +39,7 @@ namespace twodo.say
             {
                 Console.WriteLine(Guid.NewGuid());
             }
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
