1d2c300b6e22f21382fbb8373997961fa4b7a384
Two <Two.Logician@icloud.com>
2021-10-21 13:59:07 +0700

    :sparkles:

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ option | description
 `a-z` | Returns a random character that is within A to Z.
 `date` | Gets a DateTime object that is set to the current date and time on this computer, expressed as the local time.
 `guid` | Initializes a new instance of the Guid structure.
+`ip` | Gets an IP address that is connected.
--- src/twodo.say/Program.cs
+++ src/twodo.say/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 
 namespace twodo.say
 {
@@ -8,38 +10,70 @@ namespace twodo.say
     {
         static void Main(string[] args)
         {
-            var commands = new List<string> { "A-Z", "DATE", "GUID" };
+            var commands = new List<string> { "a-z", "date", "ip", "guid" };
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
             if (!args.Any())
             {
-                Console.WriteLine("Usage: two [options]");
-                Console.ForegroundColor = ConsoleColor.White;
+                WriteYellow("Usage: two [options]");
                 return;
             }
-            Console.ForegroundColor = ConsoleColor.Red;
-            if (!args.Any(x => commands.Contains(x.ToUpper())))
+            if (!args.Any(x => commands.Contains(x)))
             {
-                Console.WriteLine("Unknow Option");
-                Console.ForegroundColor = ConsoleColor.White;
+                WriteRed("Unknow Option");
                 return;
             }
-            Console.ForegroundColor = ConsoleColor.Green;
-            if (args.Any(x => x.ToUpper() == "A-Z"))
+            if (args.Any(x => x == "a-z"))
             {
                 var random = new Random();
                 var cha = (char)random.Next('A', 'Z');
-                Console.WriteLine(cha);
+                WriteGreen(cha);
             }
-            if (args.Any(x => x.ToUpper() == "DATE"))
+            if (args.Any(x => x == "date"))
             {
-                Console.WriteLine(DateTime.Now);
+                WriteGreen(DateTime.Now);
             }
-            if (args.Any(x => x.ToUpper() == "GUID"))
+            if (args.Any(x => x == "ip"))
             {
-                Console.WriteLine(Guid.NewGuid());
+                try
+                {
+                    var ip = Dns.GetHostEntry(Environment.MachineName).AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+                    WriteGreen(ip);
+                }
+                catch (Exception ex)
+                {
+                    WriteRed(ex.Message);
+                }
             }
-            Console.ForegroundColor = ConsoleColor.White;
+            if (args.Any(x => x == "guid"))
+            {
+                WriteGreen(Guid.NewGuid());
+            }
+        }
+
+        static void SetGray()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static void WriteGreen(object value)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(value);
+            SetGray();
+        }
+
+        static void WriteRed(object value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(value);
+            SetGray();
+        }
+
+        static void WriteYellow(object value)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(value);
+            SetGray();
         }
     }
 }
